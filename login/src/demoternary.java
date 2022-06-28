582adceb3eb8bd79b6ff01bf73e0dad3d9fda4ce
swath <swath@LAPTOP-D0UAAFCL>
2022-06-28 10:51:33 +0530

    0

---
+++ login/src/demoternary.java
@@ -0,0 +1,14 @@
+
+public class demoternary {
+
+	public static void main(String[] args) {
+int x = 11;
+		
+		x = (10 == x) ? 1 : 0;   //  condition ?: True:False
+		
+		System.out.println(x);
+     
+	}
+
+}
+
