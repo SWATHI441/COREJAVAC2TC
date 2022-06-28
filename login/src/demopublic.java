478502d403e7b284fcd44eb7215002147d3fb7d5
swath <swath@LAPTOP-D0UAAFCL>
2022-06-28 11:31:06 +0530

    2 3 4 5 6 7 8 9 10

---
+++ login/src/demopublic.java
@@ -0,0 +1,15 @@
+
+public class demopublic {
+
+	public static void main(String[] args) {
+        for(int i=1; i<=10; i++)
+        {
+       	 if(i == 1)
+       	 {
+       		 continue;   // break;
+       	 }
+       	 System.out.println(i);
+        }
+	}
+}
+
