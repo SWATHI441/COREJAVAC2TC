11731b337fba2eee523c3cb698680c7fc008bc27
swath <swath@LAPTOP-D0UAAFCL>
2022-06-28 11:28:38 +0530

    i=7 and j=14 i=8 and j=16 i=9 and j=18 i=2 and j=2 i=3 and j=3 i=4 and j=4 i=5 and j=5 i=6 and j=6 i=7 and j=7 i=8 and j=8 i=9 and j=9 i=10 and j=10 i=11 and j=11

---
+++ login/src/classdemo.java
@@ -0,0 +1,30 @@
+
+public class classdemo {
+
+	public static void main(String[] args) {
+		  // for loop
+		  for(int i=1,j=1; i<10||j<10;i++,j++)
+		  { 
+			  j++;
+			  System.out.println("i="+i+" and j="+j); 
+		  }
+
+
+		  // while loop 
+			  int i=1,j=1;
+			  while(i<10 || j<10)
+			  { 
+				  i++; j++;
+			      System.out.println("i="+i+" and j="+j); 
+			  }	
+			
+		// Do while loop
+			int a = 1, jb= 1;
+			do {
+				i++;
+				j++;
+				System.out.println("i=" + i + " and j=" + j);
+			    } while (i < 10 || j < 10);
+		}
+	}
+
